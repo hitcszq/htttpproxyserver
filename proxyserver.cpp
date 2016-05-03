b54835e19b44f957bf634bc3b83cbebccba55619
583101017@qq.com <583101017@qq.com>
2016-05-03 23:20:43 +0800

    5.3

--- proxyserver.cpp
+++ proxyserver.cpp
@@ -12,6 +12,7 @@
 struct HttpHeader{
 	char method[4];
 	char url[1024];
+	char host[1024];
 	char cookie[1024 * 10];
 	HttpHeader(){
 		ZeroMemory(this, sizeof(HttpHeader));
@@ -87,6 +88,50 @@ BOOL InitSocket()
 	}
 	ProxyServerAddr.sin_family = AF_INET;
 	ProxyServerAddr.sin_port = htons(ProxyPort);
-	ProxyServerAddr.sin_addr.S_un.S_addr=
+	ProxyServerAddr.sin_addr.S_un.S_addr = INADDR_ANY;//??????
+	if (bind(proxyServer, (SOCKADDR*)ProxyServerAddr, sizeof(SOCKADDR)) == SOCKET_ERROR)
+	{
+		printf("bind error \n");
+		return FALSE;
+	}
+	if (listen(proxyServer, SOMAXCONN) == SOCKET_ERROR){//SOMAXCONN 128最大监听队列长度
+		printf("listen error\n");
+		return FALSE;
+	}
+	return TRUE;
+}
 
+unsigned int __stdcall ProxyThread(LPVOID lpParameter)
+{
+	char Buffer[MAXSIZE];
+	char* CacheBuffer;
+	ZeroMemory(Buffer, MAXSIZE);
+	SOCKADDR_IN clientAddr;//客户端套接字 地址
+	int recvSize;
+	recvSize = recv(((ProxyParam *)lpParameter)->clientSocket, Buffer, MAXSIZE, 0);//接受客户端发送的报文
+	if (recvSize <= 0)
+	{
+		goto error;
+	}
+	HttpHeader* httpHeader = new HttpHeader();
+	CacheBuffer = new char[recvSize + 1];
+	memcpy(CacheBuffer, Buffer, recvSize);//将接受到的报文拷贝到CacheBuffer中
+	ParseHttpHead(CacheBuffer, httpHeader);
+	delete CacheBuffer;
+	if (!ConnectToServer(&((ProxyParam *)lpParameter)->serverSocket, httpHeader->host));
+}
+
+void ParseHttpHead(char *buffer, HttpHeader* httpHeader)
+{
+	char *p;
+	char * ptr;
+	const char* delim = "\r\n";//换行标志
+}
+
+BOOL ConnectToServer(SOCKET *serverSocket, char *host)
+{
+	sockaddr_in serverAddr;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(HTTP_PORT);
+	HOSTENT *hostent=
 }
